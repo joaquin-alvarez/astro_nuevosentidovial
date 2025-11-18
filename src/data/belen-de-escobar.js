34c0e18c46416267588c08ff5f6fcbfedf445ff6
L15451 <alvarezjoaquine@gmail.com>
2025-11-18 17:55:01 +0000

    feat: initial commit, boilerplate and skeleton

---
+++ src/data/belen-de-escobar.js
@@ -0,0 +1,187 @@
+export const belenDeEscobarData = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Sarmiento",
+        "description": "Entre Av. de los Inmigrantes y Los Lazaristas"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [-58.7923, -34.3485],
+          [-58.7880, -34.3485]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Bernardo de Irigoyen",
+        "description": "De Los lazaristas a Av. de los Inmigrantes"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [-58.7880, -34.3505],
+          [-58.7923, -34.3505]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Pellegrini",
+        "description": "Entre Estrada y César Díaz"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [-58.7910, -34.3525],
+          [-58.7890, -34.3525]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Los Lazaristas",
+        "description": "De Sarmiento a Pellegrini"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [-58.7880, -34.3485],
+          [-58.7880, -34.3525]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Independencia",
+        "description": "De Los Lazaristas a Av. Tapia de Cruz"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [-58.7880, -34.3465],
+          [-58.7850, -34.3465]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Independencia",
+        "description": "De Travi a Colectora Este"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [-58.7830, -34.3465],
+          [-58.7800, -34.3465]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "César Díaz",
+        "description": "Entre Colón y Moreno"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [-58.7890, -34.3525],
+          [-58.7890, -34.3545]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Belgrano",
+        "description": "De Don Bosco a Av. 25 de Mayo"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [-58.7860, -34.3505],
+          [-58.7840, -34.3505]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "San Lorenzo",
+        "description": "Entre Av. Tapia de Cruz y Don Bosco"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [-58.7850, -34.3485],
+          [-58.7830, -34.3485]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Gelves",
+        "description": "De Libertad a Brussi"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [-58.7870, -34.3545],
+          [-58.7850, -34.3545]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "General Paz",
+        "description": "Entre Brussi y Libertad"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [-58.7850, -34.3565],
+          [-58.7870, -34.3565]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Moreno",
+        "description": "De Estrada a Av. Tapia de Cruz"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [-58.7910, -34.3545],
+          [-58.7850, -34.3545]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Belén",
+        "description": "Entre Libertad y Av. San Martín"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [-58.7870, -34.3585],
+          [-58.7840, -34.3585]
+        ]
+      }
+    }
+  ]
+};
